96711778cb2a7c6e262797eec9066bac507cbd2a
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-20 15:25:54 -0500

    Initial Commit

---
+++ LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Iurie Guzun on 2021-01-20.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
